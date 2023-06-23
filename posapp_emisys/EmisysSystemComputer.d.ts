4e5c3e0709c9991e836bf42125d90db86a3d8541
Frederic Marchal <fmarchal@perso.be>
2023-06-23 11:07:22 +0200

    Null can be returned for every computer element.

--- posapp_emisys/EmisysSystemComputer.d.ts
+++ posapp_emisys/EmisysSystemComputer.d.ts
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 
 export default class EmisysSystemComputer {
-  getComputerName(): string;
-  getUuid(): string;
-  getMacAddress(): string;
-  getIpAddress(): string;
-  getManufacturer(): string;
-  getOperatingSystem(): string;
-  getModel(): string;
+  getComputerName(): string | null;
+  getUuid(): string | null;
+  getMacAddress(): string | null;
+  getIpAddress(): string | null;
+  getManufacturer(): string | null;
+  getOperatingSystem(): string | null;
+  getModel(): string | null;
 }
