7a8cefb7ff0d3750918fc8dcbf8a00225a961fd6
Victor Bonnet <vbonnet@emisys.com>
2023-08-23 10:55:03 +0200

    Add extra content to logs

--- posapp_emisys/EmisysLogsManager.d.ts
+++ posapp_emisys/EmisysLogsManager.d.ts
@@ -2,26 +2,30 @@ declare interface LogsModel {
   dateTime: string;
   type: 'info'|'warning'|'error';
   message: string;
+  extra?: unknown;
 }
 
 export default class EmisysLogsManager {
   /**
    * Add info message to logs
    * @param message
+   * @param extra
    */
-  addInfo(message: string,): void;
+  addInfo(message: string, extra?:unknown): void;
 
   /**
    * Add warning message to logs
    * @param message
+   * @param extra
    */
-  addWarning(message: string,): void;
+  addWarning(message: string, extra?:unknown): void;
 
   /**
    * Add error message to logs
    * @param message
+   * @param extra
    */
-  addError(message: string,): void;
+  addError(message: string, extra?:unknown): void;
 
   /**
    * Return logs LogsModel[]
