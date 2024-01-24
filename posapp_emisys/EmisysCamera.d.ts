da9e15f52594b0bd1b2b902f697e5f87a948e93f
Victor Bonnet <vbonnet@emisys.com>
2024-01-24 15:48:03 +0100

    Implement laser interface

--- posapp_emisys/EmisysCamera.d.ts
+++ posapp_emisys/EmisysCamera.d.ts
@@ -1,4 +1,3 @@
-
 export default class EmisysCamera {
   /**
    * Open barcode scanner
---
+++ posapp_emisys/EmisysLaser.d.ts
@@ -0,0 +1,11 @@
+export default class EmisysLaser {
+  /**
+   * Initialize laser
+   */
+  init(): void;
+
+  /**
+   * Get barcode scanned
+   */
+  getBarcodeScanned(): string;
+}
