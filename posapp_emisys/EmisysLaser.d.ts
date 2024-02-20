60561c8851bd59dcc8e234bfcb548bf870d50c2d
Victor Bonnet <vbonnet@emisys.com>
2024-02-20 11:13:16 +0100

    Add laser interface (again)

---
+++ posapp_emisys/EmisysLaser.d.ts
@@ -0,0 +1,11 @@
+export default class EmisysLaser {
+  /**
+   * Init laser listener
+   */
+  initBarcodeScannedListener(): void;
+
+  /**
+   * Get scanned barcode to display balance
+   */
+  getBarcodeBalanceScanned(): string;
+}
