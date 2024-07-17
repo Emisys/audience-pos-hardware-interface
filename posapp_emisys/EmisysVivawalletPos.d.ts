c8ebca5a31df0899aee4593bb16140ea522f7330
Victor Bonnet <vbonnet@emisys.com>
2024-07-17 22:14:01 +0200

    Implement viva payment V2

--- posapp_emisys/EmisysVivawalletPos.d.ts
+++ posapp_emisys/EmisysVivawalletPos.d.ts
@@ -1,6 +1,8 @@
 export default class EmisysVivawalletPos {
   startSale(clientTransactionId: string, amount: number): void;
+  startSaleV2(url: string): void;
   getTransactionDetails(clientTransactionId: string, terminalId: string): void;
+  getTransactionDetailsV2(url: string): void;
   pollingVivawalletResult(
     callback: (result: any) => void,
     number: number
