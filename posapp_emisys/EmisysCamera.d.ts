db3e421af7801d50bd1ed416b9d2395d7c9db405
Frederic Marchal <fmarchal@emisys.eu>
2025-07-26 17:55:45 +0200

    Add function to check if native barcode reader is available.
    Version 1.0.31.

--- posapp_emisys/EmisysCamera.d.ts
+++ posapp_emisys/EmisysCamera.d.ts
@@ -1,5 +1,12 @@
 export default class EmisysCamera {
   /**
+   * Is the device equiped with a barcode scanner?
+   * Note that this is the react-native camera. The webview may still offer its
+   * own navigator.mediaDevices.enumerateDevices().
+   */
+  hasBarcodeScanner(): boolean;
+
+  /**
    * Open barcode scanner
    */
   openBarcodeScanner(): void;
